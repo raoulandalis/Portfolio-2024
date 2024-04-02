26c573703793de6e545796df156887ac31c1f227
Raoul Andalis <raoulandalis1@yahoo.com>
2024-04-02 18:25:35 -0400

    installed tailwind and utility classes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export const App = () => {
     return (
-        <div>App</div>
+        <div>
+            <h1 className="text-3xl font-bold underline text-red-500">
+                Hello world!
+            </h1>
+        </div>
     )
 }
