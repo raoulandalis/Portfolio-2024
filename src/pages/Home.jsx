40016f70ee304995ea072b5d4909a3dd3df029f0
Raoul Andalis <raoulandalis1@yahoo.com>
2024-04-08 17:16:04 -0400

    install react-three drei

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,11 @@ const Home = () => {
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
-
+          <directionalLight/>
+          <ambientLight/>
+          <pointLight/>
+          <spotLight/>
+          <hemisphereLight/>
         </Suspense>
       </Canvas>
     </section>
