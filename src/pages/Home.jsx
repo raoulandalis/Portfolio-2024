a9343da9a0f73c20712e084c2853cb2d0bcf5b81
Raoul Andalis <raoulandalis1@yahoo.com>
2024-04-08 16:39:02 -0400

    install react-three-fiber

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-
+import { Suspense } from 'react'
+import { Canvas, useLoader } from '@react-three/fiber'
+import Loader from '../components/Loader'
+{/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+        POPUP
+      </div> */}
+//install threejs fiber - react renderer for threejs (allows you to use threejs in react)
+//all 3d scenes and lights are rendered within the Canvas component
+//suspense is a wrapper that displays a "fallback" that will display until the children are finished loading
 const Home = () => {
   return (
     <section className='w-full h-screen relative'>
-      {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
-        POPUP
-      </div> */}
+      <Canvas
+        className='w-full h-screen relative bg-transparent'
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+
+        </Suspense>
+      </Canvas>
     </section>
   )
 }
