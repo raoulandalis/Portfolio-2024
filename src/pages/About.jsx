d8ebac1ebf651896138f0fc2c2da4e4ee0a68fb6
Raoul Andalis <raoulandalis1@yahoo.com>
2024-04-29 18:48:50 -0400

    structuring page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,20 +2,26 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='w-full h-screen relative'>
-      <div className='flex flex-row justify-around m-36 p-52 bg-blue-50'>
-        <div>Past</div>
-        <div>Second div</div>
-      </div>
-      <div className='flex flex-row justify-around m-36 p-52 gap bg-red-50'>
-        <div>Second div</div>
-        <div>Present</div>
-      </div>
-      <div className='flex flex-row justify-around m-36 p-52 bg-green-50'>
-        <div>Future</div>
-        <div>Second div</div>
-      </div>
-    </div>
+    <main className='w-full h-screen relative'>
+      <section className='flex flex-row justify-around m-36 bg-blue-50'>
+        <article className='flex gap-52'>
+          <h2>Past</h2>
+          <p>Before diving into the world of coding, I actually had a different path—I started out in healthcare. After earning my bachelor's at the University of Florida, I spent some time as an EMT, where I learned the importance of quick thinking and problem-solving under pressure.</p>
+        </article>
+      </section>
+      <section className='flex flex-row justify-around m-36 bg-red-50'>
+        <article className='flex gap-52'>
+          <p>However, I felt a calling towards something different. I discovered my love for creating and building, and that's what brought me to where I am now. I'm currently immersed in a full stack bootcamp, where I'm combining my previous experiences with my newfound passion for coding. It's been an exciting journey of discovery and growth!</p>
+          <h2>Present</h2>
+        </article>
+      </section>
+      <section className='flex flex-row justify-around m-36 bg-green-50'>
+        <article className='flex gap-52'>
+          <h2>Future</h2>
+          <p>As I look ahead, I'm eager to make my mark in the tech world. I want to continue honing my skills, pushing myself to become an even stronger developer. But most importantly, I'm excited to keep learning and exploring new possibilities, because in this field, there's always something new to discover!</p>
+        </article>
+      </section>
+    </main>
   )
 }
 
