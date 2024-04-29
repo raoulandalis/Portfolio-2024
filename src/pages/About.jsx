37363106403891a9972aa840d09975bb3776ca9a
Raoul Andalis <raoulandalis1@yahoo.com>
2024-04-29 18:31:24 -0400

    added styling

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,8 +3,16 @@ import React from 'react'
 const About = () => {
   return (
     <div className='w-full h-screen relative'>
-      <div className='flex flex-row justify-around m-36 gap'>
+      <div className='flex flex-row justify-around m-36 p-52 bg-blue-50'>
         <div>Past</div>
+        <div>Second div</div>
+      </div>
+      <div className='flex flex-row justify-around m-36 p-52 gap bg-red-50'>
+        <div>Second div</div>
+        <div>Present</div>
+      </div>
+      <div className='flex flex-row justify-around m-36 p-52 bg-green-50'>
+        <div>Future</div>
         <div>Second div</div>
       </div>
     </div>
