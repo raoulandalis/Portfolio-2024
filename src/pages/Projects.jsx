a513a94816c70846f74b0f520d4fd748aa3b0575
Raoul Andalis <raoulandalis1@yahoo.com>
2024-05-30 22:22:58 -0400

    started projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,7 +2,36 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <main className='w-full min-h-screen flex justify-center items-center'>
+      <div className="max-w-4xl mx-auto mt-40 my-36">
+        <h1 className='text-5xl flex justify-center mb-14'>Dive Into</h1>
+        <section className='flex flex-col md:flex-row justify-around md:justify-between mb-8 md:mb-36'>
+          <article className='flex flex-col md:flex-row gap-8 md:gap-36 items-center md:items-start'>
+            <h2 className='text-4xl flex items-center gap-2'>FearFace
+              <div>😈</div>
+            </h2>
+            <p className="max-w-md">Lorem Ipsum</p>
+          </article>
+        </section>
+        <section className='flex flex-col md:flex-row justify-around md:justify-between mb-8 md:mb-36'>
+          <article className='flex flex-col md:flex-row gap-8 md:gap-36 items-center md:items-start'>
+            <h2 className='text-4xl flex items-center gap-2 md:block sm:block lg:hidden'>Flix 🎥
+            </h2>
+            <p className="max-w-md">Lorem Ipsum</p>
+            <h2 className='text-4xl flex items-center gap-2 hidden lg:block'>Flix 🎥
+            </h2>
+          </article>
+        </section>
+        <section className='flex flex-col md:flex-row justify-around'>
+          <article className='flex flex-col md:flex-row gap-8 md:gap-36 items-center md:items-start'>
+            <h2 className='text-4xl flex items-center gap-2'>Future
+              <div>➡️</div>
+            </h2>
+            <p className="max-w-md">As I look ahead, I'm eager to make my mark in the tech world. I want to continue honing my skills, pushing myself to become an even stronger developer. But most importantly, I'm excited to keep learning and exploring new possibilities, because in this field, there's always something new to discover!</p>
+          </article>
+        </section>
+      </div>
+    </main>
   )
 }
 
