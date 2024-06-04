a826e4260803414b5f57f9ec6c8061c0c2907e94
Raoul Andalis <raoulandalis1@yahoo.com>
2024-06-03 21:12:49 -0400

    adding gifs to each project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -11,24 +11,36 @@ const Projects = () => {
             <h2 className='text-4xl flex items-center gap-2'><Link to='https://fearface.onrender.com/' target='_blank'>FearFace</Link>
               <div>😈</div>
             </h2>
-            <p className="max-w-md">Lorem Ipsum</p>
+            <img
+              src={'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHFpOHc2OGRibGpyYjM4MW9jdXd1YzVoeHBtdW81ZHpvazBxYXBwOSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/715gr12wchOLl7BO90/giphy.gif'}
+              className='-m-10'
+            ></img>
           </article>
         </section>
         <section className='flex flex-col md:flex-row justify-around md:justify-between mb-8 md:mb-36'>
           <article className='flex flex-col md:flex-row gap-8 md:gap-36 items-center md:items-start'>
             <h2 className='text-4xl flex items-center gap-2 md:block sm:block lg:hidden'><Link to='https://flix-dyz2.onrender.com/' target='_blank'>Flix 🎥</Link>
             </h2>
-            <p className="max-w-md">Lorem Ipsum</p>
+            {/* <p className="max-w-md">Lorem Ipsum</p>
+             */}
+            <img
+              src={'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHFpOHc2OGRibGpyYjM4MW9jdXd1YzVoeHBtdW81ZHpvazBxYXBwOSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/715gr12wchOLl7BO90/giphy.gif'}
+              className='-m-10'
+            ></img>
             <h2 className='text-4xl flex items-center gap-2 hidden lg:block'><Link to='https://flix-dyz2.onrender.com/' target='_blank'>Flix 🎥</Link>
             </h2>
           </article>
         </section>
         <section className='flex flex-col md:flex-row justify-around'>
           <article className='flex flex-col md:flex-row gap-8 md:gap-36 items-center md:items-start'>
-            <h2 className='text-4xl flex items-center gap-2'>ValBnb
+            <h2 className='text-4xl flex items-center gap-2'><Link to='https://raoul-airbnb.onrender.com/' target='_blank'>ValBnb</Link>
               <div>🎮</div>
             </h2>
-            <p className="max-w-md">Lorem Ipsum</p>
+            {/* <p className="max-w-md">Lorem Ipsum</p> */}
+            <img
+              src={'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHFpOHc2OGRibGpyYjM4MW9jdXd1YzVoeHBtdW81ZHpvazBxYXBwOSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/715gr12wchOLl7BO90/giphy.gif'}
+              className='-m-10'
+            ></img>
           </article>
         </section>
       </div>
