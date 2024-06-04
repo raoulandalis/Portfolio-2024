a768375ef2518ca2051f4462ceee3d23405925c8
Raoul Andalis <raoulandalis1@yahoo.com>
2024-06-03 22:35:06 -0400

    added all projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,7 +5,7 @@ const Projects = () => {
   return (
     <main className='w-full min-h-screen flex justify-center items-center'>
       <div className="max-w-4xl mx-auto mt-40 my-36">
-        <h1 className='text-5xl flex justify-center mb-14'>Dive Into...</h1>
+        <h1 className='text-5xl flex justify-center mb-20'>Dive Into...</h1>
         <section className='flex flex-col md:flex-row justify-around md:justify-between mb-8 md:mb-36'>
           <article className='flex flex-col md:flex-row gap-8 md:gap-36 items-center md:items-start'>
             <div>
@@ -26,7 +26,7 @@ const Projects = () => {
               <p className="max-w-md mt-2">A team developed, horror-themed social app where users can browse and post comments, interact with others, and explore a haunted marketplace featuring witty horror movie references. Horror fans will enjoy this playful and engaging community.</p>
             </div>
             <img
-              src={'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHFpOHc2OGRibGpyYjM4MW9jdXd1YzVoeHBtdW81ZHpvazBxYXBwOSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/715gr12wchOLl7BO90/giphy.gif'}
+              src={'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcnd2dHJmaW5id3RiempzdWw1cGM2Mjl0cGJwMXlvaW9scmZiZTNpeCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/jEOocwutlskiTvgm2O/giphy.gif'}
               className='-m-10'
             ></img>
           </article>
@@ -36,23 +36,23 @@ const Projects = () => {
           <article className='flex flex-col md:flex-row gap-8 md:gap-36 items-center md:items-start'>
             <div>
               <div className='flex flex-row gap-3'>
-                <h2 className='text-4xl flex items-center italic'>FearFace
+                <h2 className='text-4xl flex items-center italic'>Flix
                 </h2>
                 <div className='flex items-end'>
-                  <Link to='https://fearface.onrender.com/' target='_blank' className="hover:text-blue-500">
+                  <Link to='https://flix-dyz2.onrender.com/' target='_blank' className="hover:text-blue-500">
                     live
                   </Link>
                 </div>
                 <div className='flex items-end'>
-                  <Link to='https://github.com/jgodfrey324/FearFace' target='_blank' className="hover:text-blue-500">
+                  <Link to='https://github.com/raoulandalis/Flix' target='_blank' className="hover:text-blue-500">
                     github
                   </Link>
                 </div>
               </div>
-              <p className="max-w-md mt-2">A team developed, horror-themed social app where users can browse and post comments, interact with others, and explore a haunted marketplace featuring witty horror movie references. Horror fans will enjoy this playful and engaging community.</p>
+              <p className="max-w-md mt-2">An independently made, movie-app for friend groups to upload, rate, and review movies. Movie-lovers can also categorize movies by creating curated playlists all while contributing to their own profile stats.</p>
             </div>
             <img
-              src={'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHFpOHc2OGRibGpyYjM4MW9jdXd1YzVoeHBtdW81ZHpvazBxYXBwOSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/715gr12wchOLl7BO90/giphy.gif'}
+              src={'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYzh3OG9jMGFidnFtMHQ3aWowY3E0ZmZybWhpdzc4MHB6eTNiZWJqcyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/TZDBbWqlv74kNW7VMI/giphy.gif'}
               className='-m-10'
             ></img>
           </article>
@@ -62,23 +62,23 @@ const Projects = () => {
           <article className='flex flex-col md:flex-row gap-8 md:gap-36 items-center md:items-start'>
             <div>
               <div className='flex flex-row gap-3'>
-                <h2 className='text-4xl flex items-center italic'>FearFace
+                <h2 className='text-4xl flex items-center italic'>Valbnb
                 </h2>
                 <div className='flex items-end'>
-                  <Link to='https://fearface.onrender.com/' target='_blank' className="hover:text-blue-500">
+                  <Link to='https://raoul-airbnb.onrender.com/' target='_blank' className="hover:text-blue-500">
                     live
                   </Link>
                 </div>
                 <div className='flex items-end'>
-                  <Link to='https://github.com/jgodfrey324/FearFace' target='_blank' className="hover:text-blue-500">
+                  <Link to='https://github.com/raoulandalis/Valbnb' target='_blank' className="hover:text-blue-500">
                     github
                   </Link>
                 </div>
               </div>
-              <p className="max-w-md mt-2">A team developed, horror-themed social app where users can browse and post comments, interact with others, and explore a haunted marketplace featuring witty horror movie references. Horror fans will enjoy this playful and engaging community.</p>
+              <p className="max-w-md mt-2">A Airbnb inspired app based off the theme of the popular game "Valorant", where gaming enthusiasts can create listing of locations they're renting out and leave reviews of places they've visted.</p>
             </div>
             <img
-              src={'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHFpOHc2OGRibGpyYjM4MW9jdXd1YzVoeHBtdW81ZHpvazBxYXBwOSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/715gr12wchOLl7BO90/giphy.gif'}
+              src={'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMDlsNnRhNWNjajNxb2RxbXNlM3p5Z3hrbnA3cjA0aHAxaWdiczFkOSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/tNHOL9qG3QMcvQ096W/giphy.gif'}
               className='-m-10'
             ></img>
           </article>
@@ -89,32 +89,3 @@ const Projects = () => {
 }
 
 export default Projects
-
-
-// < section className = 'flex flex-col md:flex-row justify-around md:justify-between mb-8 md:mb-36' >
-//   <article className='flex flex-col md:flex-row gap-8 md:gap-36 items-center md:items-start'>
-//     <div>
-//       <h2 className='text-4xl flex items-center gap-2 md:block sm:block lg:hidden'><Link to='https://flix-dyz2.onrender.com/' target='_blank'>Flix 🎥</Link>
-//       </h2>
-//       {/* <p className="max-w-md">Lorem Ipsum</p> */}
-//     </div>
-//     <img
-//       src={'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHFpOHc2OGRibGpyYjM4MW9jdXd1YzVoeHBtdW81ZHpvazBxYXBwOSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/715gr12wchOLl7BO90/giphy.gif'}
-//       className='-m-10'
-//     ></img>
-//     <h2 className='text-4xl flex items-center gap-2 hidden lg:block'><Link to='https://flix-dyz2.onrender.com/' target='_blank'>Flix 🎥</Link>
-//     </h2>
-//   </article>
-//       </section >
-// <section className='flex flex-col md:flex-row justify-around'>
-//   <article className='flex flex-col md:flex-row gap-8 md:gap-36 items-center md:items-start'>
-//     <h2 className='text-4xl flex items-center gap-2'><Link to='https://raoul-airbnb.onrender.com/' target='_blank'>ValBnb</Link>
-//       <div>🎮</div>
-//     </h2>
-//     {/* <p className="max-w-md">Lorem Ipsum</p> */}
-//     <img
-//       src={'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHFpOHc2OGRibGpyYjM4MW9jdXd1YzVoeHBtdW81ZHpvazBxYXBwOSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/715gr12wchOLl7BO90/giphy.gif'}
-//       className='-m-10'
-//     ></img>
-//   </article>
-// </section>
