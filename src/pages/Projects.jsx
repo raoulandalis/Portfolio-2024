a8a3878bc0a1067c0b52fe6c9584b1a0b026dcc9
Raoul Andalis <raoulandalis1@yahoo.com>
2024-06-04 21:23:27 -0400

    created individual skills component to declutter

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Skills from './Skills'
 import { Link } from 'react-router-dom'
 
 const Projects = () => {
@@ -82,37 +83,7 @@ const Projects = () => {
           </article>
         </section>
 
-        <h1 className='text-5xl flex justify-center mb-16'>Skills</h1>
-
-        <section className='cursor-pointer animate-slideUp'>
-          <h2 className='text-3xl'>Languages</h2>
-          <div className='flex gap-5'>
-            <p className='text-xl mt-4 border border-blue-500 p-2 hover:shadow-md'>JavaScript</p>
-            <p className='text-xl mt-4 border border-blue-500 p-2 hover:shadow-md'>Python</p>
-          </div>
-        </section>
-
-        <section className='cursor-pointer animate-slideUp'>
-          <h2 className='text-3xl mt-10'>Front End</h2>
-          <div className='flex gap-5'>
-            <p className='text-xl mt-4 border border-blue-500 p-2 hover:shadow-md'>React</p>
-            <p className='text-xl mt-4 border border-blue-500 p-2 hover:shadow-md'>Next.js</p>
-            <p className='text-xl mt-4 border border-blue-500 p-2 hover:shadow-md'>Redux</p>
-            <p className='text-xl mt-4 border border-blue-500 p-2 hover:shadow-md'>HTML/CSS</p>
-            <p className='text-xl mt-4 border border-blue-500 p-2 hover:shadow-md'>TailWindCSS</p>
-          </div>
-        </section>
-
-        <section className='cursor-pointer animate-slideUp'>
-          <h2 className='text-3xl mt-10'>Back End</h2>
-          <div className='flex gap-5'>
-            <p className='text-xl mt-4 border border-blue-500 p-2 hover:shadow-md'>Node.js</p>
-            <p className='text-xl mt-4 border border-blue-500 p-2 hover:shadow-md'>Express</p>
-            <p className='text-xl mt-4 border border-blue-500 p-2 hover:shadow-md'>Flask</p>
-            <p className='text-xl mt-4 border border-blue-500 p-2 hover:shadow-md'>SQL</p>
-            <p className='text-xl mt-4 border border-blue-500 p-2 hover:shadow-md'>PostgreSQL</p>
-          </div>
-        </section>
+        <Skills/>
 
 
 
