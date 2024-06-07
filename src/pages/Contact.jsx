d53a6c1247ac59c016921da6b772f0a03ab3c3ea
Raoul Andalis <raoulandalis1@yahoo.com>
2024-06-07 13:59:09 -0400

    adding logos

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,18 +2,26 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <main className='w-full min-h-screen flex justify-center bg-blue-200'>
+    <main className='w-full min-h-screen flex justify-center bg-gray-800'>
       <div className="max-w-4xl mx-auto my-36 justify-center">
-        <h1 className='text-5xl flex justify-center mb-14'>Want to connect?</h1>
+        <h1 className='text-5xl flex justify-center mb-14 text-blue-500'>Want to connect?</h1>
 
-        <div className='flex flex-col items-center gap-6 text-2xl animate-slideUp'>
+        <div className='flex flex-col items-center gap-6 text-2xl animate-slideUp text-white'>
           <section>
             <a href="mailto:raoulandalis@gmail.com" className="hover:text-blue-500">Email</a>
           </section>
-          <section>
+          <section className='flex gap-2'>
+
+            <img src="https://devicon-website.vercel.app/api/linkedin/original.svg" className='h-8'></img>
+
             <a href='https://www.linkedin.com/in/raoul-andalis/' target='_blank' rel='noopener noreferrer' className='hover:text-blue-500'>LinkedIn</a>
           </section>
-          <section>
+          <section className='flex gap-2'>
+
+
+            <img src="https://devicon-website.vercel.app/api/github/original.svg?color=%232C2CA3" className='h-8'></img>
+
+
             <a href='https://github.com/raoulandalis' target='_blank' rel='noopener noreferrer' className='hover:text-blue-500'>GitHub</a>
           </section>
           <section>
